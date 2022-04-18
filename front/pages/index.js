eac1d41685f1381cf98ed9edfe9e4fa0c30ff7cc
HomieKim <ghals0708@gmail.com>
2022-04-18 23:54:29 +0900

    unbuntu server install

--- front/pages/index.js
+++ front/pages/index.js
@@ -1,5 +1,5 @@
-import axios from 'axios';
 import React, { useEffect } from 'react';
+import axios from 'axios';
 import { useInView } from 'react-intersection-observer';
 import { useDispatch, useSelector } from 'react-redux';
 import { END } from 'redux-saga';
